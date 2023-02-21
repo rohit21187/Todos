45aaa3d8ff080bbb4b2a68a4b8fbc4cacf173e0e
Rohit <rohit21187@gmail.com>
2023-02-22 04:25:18 +0530

    read todo from db implemented

--- Backend/handler.js
+++ Backend/handler.js
@@ -1,12 +1,22 @@
 'use strict';
 const create = require('./create.js')
+const readAll= require('./readAll.js')
 
 module.exports.create = async (event,context, callback) => {
-  create(event,(error,result)=>{
-    const response={
+  await create(event, (error, result) => {
+    const response = {
       statusCode: 200,
       body: JSON.stringify(result),
     }
     context.succeeded(response)
   })
-};
+}
+module.exports.readAll = async (event, context, callback) => {
+  await readAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    }
+    context.succeeded(response);
+  })
+}
